69c6ff21224e9cc76f8b6f0496f88dc2ea6f2966
s-sandra <aleksandra.shtabnaya@gmail.com>
2017-05-10 18:21:59 -0400

    Completed refactor for file parsing by transferring methods into Schedule and Course.

--- src/Course.java
+++ src/Course.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 /**
-*
+* This class stores attributes for a class in a schedule.
 * @author Sandra Shtabnaya
 */
 
@@ -18,39 +18,216 @@ public class Course {
 	private String location;
 	private String conflictingCourse;
 	private String day;
+	private int differingTimes; //stores the amount of different meeting times.
+	private Schedule schedule;
 
 	/**
 	 * Creates a new course from a schedule file.
 	 * @param file the Scanner reading the .csv file.
+	 * @param sch the Schedule the course belongs to.
 	 * @throws IllegalFileException if the schedule file has improper formatting.
 	 */
-	public Course(Scanner file) throws IllegalFileException{
+	public Course(Scanner file, Schedule sch) throws IllegalFileException{
+		schedule = sch;
 		String line = file.nextLine();
-		int totalDifferingClassTimes = countMeetingTimes(line, ',') - 5;
+		differingTimes = countMeetingTimes(line);
 
-		//if the given class in the line has a single meeting time.
-		if(totalDifferingClassTimes == 0){
-			parseCourse(line, 1);
+		//if the given class has a single meeting time.
+		if(differingTimes == 0){
+			differingTimes = 1;
 		}
 
 		if((line.toUpperCase().contains("ONLINE") || line.toUpperCase().contains("TBA"))
-				&& totalDifferingClassTimes != -2){
+				&& differingTimes != -2){
 			file.close();
 			throw new IllegalFileException("Check commas.");
 		}
-		else if(totalDifferingClassTimes < 0 && (!line.toUpperCase().contains("ONLINE")
+		else if(differingTimes < 0 && (!line.toUpperCase().contains("ONLINE")
 				&& !line.toUpperCase().contains("TBA"))){
 			file.close();
 			throw new IllegalFileException("Check commas.");
 		}
 
-		//if the given class in the line has several meeting times.
-		parseCourse(line, totalDifferingClassTimes);
+		parseCourse(file);
 	}
 
-	public void setCredits(int cred){
-		credits = cred;
+
+	/**
+	 * This helper method calculate the amount of differing class times
+	 * for a course by counting the number of commas separating dates and
+	 * class times.
+	 * @param line the line of the csv file containing the course information.
+	 * @return the number of differing meeting times.
+	 */
+	private int countMeetingTimes(String line){
+		int classTimesCount = 0;
+
+		for(int i = 0; i < line.length(); i++){
+			if(line.charAt(i) == ','){
+				classTimesCount++;
+			}
+		}
+
+		//subtracts the number of commas in a description with exactly one meeting time.
+		return classTimesCount - 5;
 	}
+
+
+	/**
+	 * This helper method parses a line from the schedule file and
+	 * sets the appropriate values for the class.
+	 * @param file the Scanner reading the csv file.
+	 */
+	private void parseCourse(Scanner file){
+
+		file.useDelimiter(",");
+
+		//takes in the first four strings representing the
+		//course name, title, location and credits.
+		courseName = file.next();
+		courseTitle = file.next().trim();
+		credits = Integer.valueOf(file.next().trim());
+
+		parseLocations(file);
+
+		//if the class is online, does not look for dates.
+		if(location.toUpperCase().equals("ONLINE")){
+			courseTitle += " ONLINE";
+			return;
+		}
+		else if(location.toUpperCase().equals("TBA")){
+			courseTitle += " TBA";
+			return;
+		}
+
+		file.useDelimiter(",|-");
+		parseTimes(file);
+
+	}
+
+	/**
+	 * This helper method reads in the string
+	 * containing a class' meeting times.
+	 * @param file the Scanner reading the csv file.
+	 */
+	private void parseTimes(Scanner file){
+		String meetingDays;
+		String startTime;
+		String endTime;
+
+		//searches for more dates and times based on the
+		//amount of differing class times.
+		for(int i = 0; i < differingTimes; i++) {
+			meetingDays = file.next().trim();
+			startTime = file.next();
+			endTime = file.next();
+
+			//while the meetingDays string has more week days
+			while (meetingDays.length() > 0) {
+
+				//gets the first character in dates.
+				setDay(chopMeetingDays(meetingDays));
+
+				startHour = parseHour(startTime);
+				startMinute = parseMinute(startTime);
+				setStartTime(isMorning(startTime));
+
+				endHour = parseHour(endTime);
+				endMinute = parseMinute(endTime);
+				setEndTime(isMorning(endTime));
+
+				//adds the course to the schedule.
+				schedule.addCourse(this);
+
+				//if the meetingDays string still has another
+				//week day
+				if (meetingDays.length() > 1) {
+					meetingDays = meetingDays.substring(1);
+				} else {
+					meetingDays = "";
+				}
+			}
+		}
+	}
+
+
+	/**
+	 * This helper method determines if a given
+	 * time of day is in the morning.
+	 * @param s AM or PM.
+	 */
+	private boolean isMorning(String s){
+		s = s.toUpperCase();
+		String timeOfDay = s;
+
+		if(timeOfDay.contains("P")){
+			return false;
+		}
+		return true;
+	}
+
+
+	/**
+	 * This helper method takes in the meeting days of the
+	 * course corresponding to a class time. Each character
+	 * represents a single day of the week, with Thursday denoted
+	 * as "R."
+	 */
+	private char chopMeetingDays(String dates){
+		if(dates.length() > 0){
+			return dates.charAt(0);
+		}
+		return ' ';
+	}
+
+
+	/**
+	 * This helper method takes in a string containing
+	 * either the class start time or end time. It parses the line
+	 * to return the hour in integer form.
+	 * @param s the String containing a time.
+	 */
+	private int parseHour(String s){
+		Scanner parseHour = new Scanner(s);
+		parseHour.useDelimiter(":");
+		int hour = Integer.valueOf(parseHour.next().trim());
+		parseHour.close();
+		return hour;
+	}
+
+
+	/**
+	 * This helper method takes in a string containing
+	 * either the class start time or end time. It parses the line
+	 * to return the amount of minutes from the beginning of the hour.
+	 * @param s the String containing a time.
+	 */
+	private static int parseMinute(String s){
+		s = s.toUpperCase();
+		Scanner parseMin = new Scanner(s);
+		parseMin.useDelimiter(":|AM|PM");
+		parseMin.next();
+		int minute = Integer.valueOf(parseMin.next().trim());
+		parseMin.close();
+		return minute;
+	}
+
+
+	/**
+	 * This helper method reads in the string
+	 * containing a class' location.
+	 * @param file the Scanner reading the csv file.
+	 */
+	private void parseLocations(Scanner file){
+		String hall = file.next();
+
+		location = hall;
+
+		while(file.hasNext()){
+			location = hall + " " + file.next();
+		}
+	}
+
 	
 	public void setDay(char date){
 		if(date == 'M'){
@@ -73,10 +250,7 @@ public class Course {
 		}
 	}
 	
-	public void setStartTime(int hour, int min, boolean isMorning){
-		startHour = hour;
-		startMinute = min;
-		
+	public void setStartTime(boolean isMorning){
 		if(isMorning){
 			startTimeOfDay = "AM";
 		}
@@ -85,10 +259,7 @@ public class Course {
 		}
 	}
 	
-	public void setEndTime(int hour, int min, boolean isMorning){
-		endHour = hour;
-		endMinute = min;
-		
+	public void setEndTime(boolean isMorning){
 		if(isMorning){
 			endTimeOfDay = "AM";
 		}
@@ -96,28 +267,218 @@ public class Course {
 			endTimeOfDay = "PM";
 		}
 	}
-	
-	public void setName(String name){
-		courseName = name;
-	}
-	
-	public void setTitle(String title){
-		courseTitle = title;
-	}
-	
-	public void setLoc(String place){
-		location = place;
-	}
-	
+
+
 	/**
-	 * getConflict - this method is used to 
-	 * store the name of the class it conflicts
+	 * Determines whether the passed course conflicts with
+	 * this course.
+	 * @param course the course in question.
+	 * @return whether a time conflict exists between the passed course
+	 * and this course.
+	 */
+	public boolean conflictsWith(Course course) {
+		boolean conflicting = false;
+
+		//stores the details of the class we are attempting to add
+		int addingStartHour = course.getStartHr();
+		int addingEndHour = course.getEndHr();
+		int addingStartMin = course.getStartMin();
+		int addingEndMin = course.getEndMin();
+		String addingStartTime = course.getStartTimeOfDay();
+		String addingEndTime = course.getEndTimeOfDay();
+
+		//if the current class ends at the same time of day
+		//as the adding class starts.
+		if (endTimeOfDay.equals(addingStartTime)) {
+
+			//if the current class end hour is the same
+			//as the adding class start hour
+			if (endHour == addingStartHour) {
+
+				//if the current class ends after or at the same
+				//time as the adding class begins
+				if (endMinute >= addingStartMin) {
+					conflicting = true;
+				}
+			}
+
+			//if the current class ends after the adding class begins
+			else if (endHour != 12 && endHour > addingStartHour) {
+
+				//if the adding class ends before the current class ends
+				if (addingEndHour > endHour) {
+					conflicting = true;
+				}
+			}
+		}
+
+		//if the current class starts at the same time of
+		//day as the adding class
+		if (startTimeOfDay.equals(addingEndTime)) {
+
+			//if the current class start hour is the same
+			//as the adding class end hour
+			if (addingEndHour == startHour) {
+
+				//if the adding class ends after or at the same
+				//time as the added class begins
+				if (addingEndMin >= startMinute) {
+					conflicting = true;
+				}
+			}
+
+			//if the current class starts before the adding class ends
+			else if (startHour != 12 && startHour < addingEndHour) {
+
+				//if the current class ends after the adding
+				//class begins
+				if (endHour > addingStartHour) {
+					conflicting = true;
+				}
+			}
+		}
+
+		//if both classes end at the same time of day
+		if (endTimeOfDay.equals(addingEndTime)) {
+
+			//if both classes end at the same hour
+			if (endHour == addingEndHour) {
+
+				//if the current class ends after or at the
+				//same time as the adding class ends
+				if (endMinute >= addingEndMin) {
+					conflicting = true;
+				}
+			}
+		}
+
+		//if both classes start at the same time of day
+		if (startTimeOfDay.equals(addingStartTime)) {
+
+			//if both classes start at the same hour
+			if (startHour == addingStartHour) {
+
+				//if the current class starts before or at
+				//the same minute as the adding class
+				if (addingStartMin >= startMinute) {
+					conflicting = true;
+				}
+			}
+		}
+
+		//accounts for a starting hour of noon.
+		if (startHour == 12 && endHour != 12) {
+
+			//if the adding class starts before noon and ends in the middle
+			//of the current class
+			if (addingStartTime.equals("AM") && addingEndHour < endHour) {
+				conflicting = true;
+			}
+		}
+
+		if (addingStartHour == 12 && addingEndHour != 12) {
+
+			//if the current class starts before noon and ends in the middle
+			//of the adding class
+			if (startTimeOfDay.equals("AM") && endHour < addingEndHour) {
+				conflicting = true;
+			}
+		}
+
+		//if there is a conflict, stores the name of the
+		//class this course conflicts with.
+		if (conflicting) {
+			setConflict(course.getTitle());
+		}
+
+		return conflicting;
+	}
+
+
+	/**
+	 * This method is used to store the name of the class it conflicts
 	 * with, if there is a time conflict.
+	 * @param name the name of the class this course conflicts with.
 	 */
 	public void setConflict(String name){
 		conflictingCourse = name;
 	}
-	
+
+
+	/**
+	 * This method determines the length of the class in multiples of five.
+	 * @return the amount of five minutes in the class length.
+	 */
+	public int getLength(){
+		int minuteCount = 0;
+		int totalHrs = 0;
+		int totalMins = 0;
+
+		//if the interval starts and ends in the same time of day
+		if(startTimeOfDay.equals(endTimeOfDay)){
+
+			totalMins = (60 - startMinute) + endMinute;
+
+			//if the class takes 24 hours
+			if(startHour == endHour && totalMins == 60){
+				totalHrs = 23;
+			}
+
+			//if the interval starts and ends at the same hour.
+			else if(startHour == endHour){
+				totalHrs = -1;
+			}
+
+			//if the interval starts at noon, finds the
+			//distance between the end hour and noon.
+			else if(startHour == 12){
+
+				//subtracts one to account for incomplete
+				//hours due to the start and end minutes
+				totalHrs = endHour - 1;
+			}
+
+			//if the interval ends at noon, finds the
+			//distance between the start hour and noon.
+			else if(endHour == 12){
+				totalHrs = startHour - 1;
+			}
+
+			//if the class lasts more than 24 hours
+			else if(startHour > endHour){
+				totalHrs = 23 + (startHour - endHour);
+			}
+
+			else{
+				totalHrs = endHour - startHour - 1;
+			}
+		}
+
+		//if the interval starts and ends in different
+		//parts of the day
+		else{
+
+			//if the interval ends at noon, finds the
+			//distance between the start hour from noon.
+			if(endHour == 12){
+				totalHrs = 12 - startHour - 1;
+			}
+
+			//finds how long the start hour is from noon,
+			//and adds the end hour.
+			else{
+				totalHrs = (12 - startHour) + endHour - 1;
+			}
+			totalMins = (60 - startMinute) + endMinute;
+		}
+
+		minuteCount = totalHrs * 60 + totalMins;
+		minuteCount /= 5;
+
+		return minuteCount;
+	}
+
+
 	public int getCredits(){
 		return credits;
 	}
--- src/Schedule.java
+++ src/Schedule.java
@@ -8,11 +8,10 @@ import java.io.File;
  * @author Sandra Shtabnaya
  */
 public class Schedule {
-    private ArrayList<ArrayList<Course>> week = new ArrayList<ArrayList<Course>>(); //stores all the classes in a week.
-    private ArrayList<Course> conflictingCourses = new ArrayList<Course>(); //stores all the conflicting classes in the file.
-    private ArrayList<Course> invalidCourses = new ArrayList<Course>(); //stores all classes with invalid descriptions.
-    private ArrayList<String> locations = new ArrayList<String>();
-    private ArrayList<Course> classes = new ArrayList<Course>();
+    private ArrayList<ArrayList<Course>> week = new ArrayList<>(); //stores all the classes in a week.
+    private ArrayList<Course> conflictingCourses = new ArrayList<>(); //stores all the conflicting classes in the file.
+    private ArrayList<Course> invalidCourses = new ArrayList<>(); //stores all classes with invalid descriptions.
+    private ArrayList<Course> classes = new ArrayList<>();
     private int credits = 0;
 
 
@@ -22,9 +21,9 @@ public class Schedule {
      * @throws FileNotFoundException if the .csv file cannot be found in the files directory.
      * @throws IllegalFileException if the .csv file has improper formatting.
      */
-    public Schedule(String file) throws FileNotFoundException, IllegalFileException{
+    Schedule(String file) throws FileNotFoundException, IllegalFileException{
         for(int i = 0; i < 7; i++){
-            week.add(new ArrayList<Course>());
+            week.add(new ArrayList<>());
         }
 
         readSchedule(file);
@@ -41,9 +40,175 @@ public class Schedule {
         Scanner file = new Scanner(new File(fileName));
 
         while(file.hasNextLine()) {
-
-            Course course = new Course(file);
-            classes.add(course);
+            new Course(file, this);
         }
+        file.close();
+    }
+
+
+    /**
+     * This method adds a non-conflicting course to the schedule.
+     * It also updates the credit count.
+     * @param course the course to add.
+     */
+    void addCourse(Course course){
+        for(Course event : classes){
+            if(event.getTitle().equals(course.getTitle())){
+                return;
+            }
+        }
+
+        //checks to see if the class times are valid
+        if(!hasValidTime(course)){
+            return;
+        }
+
+        credits += course.getCredits();
+        classes.add(course);
+        String day = course.getDay();
+
+
+        if(day.equals("Monday") && !isConflicting(course, 0)){
+            week.get(0).add(course);
+        }
+        else if(day.equals("Tuesday") && !isConflicting(course, 1)){
+            week.get(1).add(course);
+        }
+        else if(day.equals("Wednesday") && !isConflicting(course, 2)){
+            week.get(2).add(course);
+        }
+        else if(day.equals("Thursday") && !isConflicting(course, 3)){
+            week.get(3).add(course);
+        }
+        else if(day.equals("Friday") && !isConflicting(course, 4)){
+            week.get(4).add(course);
+        }
+        else if(day.equals("Saturday") && !isConflicting(course, 5)){
+            week.get(5).add(course);
+        }
+
+        //adds the course to the list of conflicting classes
+        //if it has not been rejected before and removes
+        //the class from other weekdays.
+        else{
+            if(isNewConflict(course)){
+                conflictingCourses.add(course);
+            }
+            removeConflictingCourses(course);
+        }
+    }
+
+
+    /**
+     * This helper method removes any conflicting courses that were previously
+     * added to the schedule.
+     */
+    private void removeConflictingCourses(Course event){
+        for(int i = 0; i < classes.size(); i++){
+            int daySize = classes.size();
+
+            for(int j = 0; j < daySize; j++){
+                if(classes.get(j).getName().equals(event.getName())){
+                    classes.remove(j);
+                    daySize--;
+                }
+            }
+        }
+    }
+
+
+    /**
+     * This helper method determines if a given course conflicts with any courses
+     * in the given day.
+     * @param event the course in question.
+     * @param day the day of the week in question.
+     * @return whether the given course conflicts with an existing course in the given day.
+     */
+    private boolean isConflicting(Course event, int day){
+        ArrayList<Course> weekDay = week.get(day);
+
+        //determines if the class is not already in our list of
+        //conflicting classes
+        if(!isNewConflict(event)){
+            return true;
+        }
+
+        //goes through each class in the weekday
+        for(Course lecture: weekDay){
+            if(lecture.conflictsWith(event)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    /**
+     * This helper method determines if the given class times are valid.
+     * @param event the course in question.
+     * @return if the class doesn't last for more than five hours or
+     * doesn't start after 11 PM and before 7 AM.
+     */
+    private boolean hasValidTime(Course event){
+        boolean isValid = true;
+
+        if(event.getLength() >= 60){
+            isValid = false;
+        }
+        else if(event.getStartTimeOfDay().equals("PM") && event.getStartHr() < 12
+                && event.getStartHr() > 10){
+            isValid = false;
+        }
+        else if(event.getStartTimeOfDay().equals("AM") && (event.getStartHr() == 12 ||
+                event.getStartHr() < 7)){
+            isValid = false;
+        }
+
+        if(!isValid && isNewConflict(event)){
+            invalidCourses.add(event);
+        }
+
+        return isValid;
+    }
+
+
+    /**
+     * This helper method determines if a rejected
+     * course has already been added to the list of invalid courses.
+     * @param event the course in question.
+     * @return whether the course has been rejected before.
+     */
+    private boolean isNewConflict(Course event){
+
+        for(Course lecture : invalidCourses){
+            if(lecture.getName().equals(event.getName())){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * This helper method generates a report to append to the
+     * end of the schedule. It makes a list of all the added classes
+     * and the total number of credits.
+     */
+    private String getReport(){
+        String report = "";
+        for(Course event : classes){
+            report += event.toString() + "\n";
+        }
+
+        report += "Total Credits: " + credits;
+        return report;
+    }
+
+
+    public String toString(){
+        String display = "";
+        display += getReport();
+        return display;
     }
 }
